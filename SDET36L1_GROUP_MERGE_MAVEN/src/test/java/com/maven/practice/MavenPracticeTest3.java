c936e27f515417c02fd7d2065d5a8dbdd1cbfe93
PRANAV <PRANAV@DESKTOP-0KIQ346>
2022-07-19 11:45:53 +0530

    1st commit

---
+++ SDET36L1_GROUP_MERGE_MAVEN/src/test/java/com/maven/practice/MavenPracticeTest3.java
@@ -0,0 +1,18 @@
+package com.maven.practice;
+
+import org.testng.annotations.Test;
+
+public class MavenPracticeTest3 
+{
+	@Test
+	public void practiceMavenTest0()
+	{
+		System.out.println("This is maven practice with the help of commandLine");
+	}
+	
+	public void practiceMavenTest1()
+	{
+		System.out.println("This is another practice");
+	}
+
+}
---
+++ SDET36L1_GROUP_MERGE_MAVEN/src/test/java/com/maven/practice/PracticeTest.java
@@ -0,0 +1,22 @@
+package com.maven.practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest 
+{
+	@Test
+	public void practiceTest() {
+		System.out.println("tyss------>>test1");
+	}
+	
+	@Test
+	public void practiceTest2() {
+		System.out.println("tyss------>>test2");
+	}
+	
+	
+	public void practiceTest3() {
+		System.out.println("tyss------>>test3");
+	}
+
+}
